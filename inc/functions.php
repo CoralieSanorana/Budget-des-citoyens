d91b9e1b5e0c978f46b5bb1ff90262b3cb340c8a
funaki302 <rafanomezantsoafunakilive@gmail.com>
2025-10-18 19:06:45 +0300

    kely sisa

--- inc/functions.php
+++ inc/functions.php
@@ -29,5 +29,81 @@
         return $result_array;
     }
 
+    function get_all_recettes_fiscales_interieurs(){
+        $requete = "SELECT * FROM RecettesFiscalesInterieures";
+        $result = mysqli_query(dbconnect(),$requete);
+        $result_array = [];
+        if ($result) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $result_array[] = $row;
+            }
+        } else {
+            echo "Error: " . mysqli_error(dbconnect());
+        }
+        return $result_array;
+    }
 
+    function get_all_recette_douaniere()
+    {
+        
+        $requete = "SELECT * FROM RecettesDouanieres";
+        $result = mysqli_query(dbconnect(),$requete);
+        $result_array = [];
+        if ($result) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $result_array[] = $row;
+            }
+        } else {
+            echo "Error: " . mysqli_error(dbconnect());
+        }
+        return $result_array;
+    }
+
+    function get_all_recette_nonFisc()
+    {
+        
+         $requete = "SELECT * FROM RecettesNonFiscales";
+        $result = mysqli_query(dbconnect(),$requete);
+        $result_array = [];
+        if ($result) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $result_array[] = $row;
+            }
+        } else {
+            echo "Error: " . mysqli_error(dbconnect());
+        }
+        return $result_array;
+    }
+
+     function get_all_Dons()
+    {
+        
+         $requete = "SELECT * FROM Dons";
+        $result = mysqli_query(dbconnect(),$requete);
+        $result_array = [];
+        if ($result) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $result_array[] = $row;
+            }
+        } else {
+            echo "Error: " . mysqli_error(dbconnect());
+        }
+        return $result_array;
+    }
+
+     function get_deficit_budget()
+    {
+        
+         $requete = "SELECT * FROM DeficitBudgetaire";
+        $result = mysqli_query(dbconnect(),$requete);
+        $result_array = [];
+        if ($result) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $result_array[] = $row;
+            }
+        } else {
+            echo "Error: " . mysqli_error(dbconnect());
+        }
+        return $result_array;
+    }
 ?>
